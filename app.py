df5032b9a6d7123ce0a6acdf80f04c60866c55e3
denis-hwk <68127514+denis-hwk@users.noreply.github.com>
2025-12-07 20:31:57 -0300

    Criação dos Requirements e do App

---
+++ app.py
@@ -0,0 +1,9 @@
+#Importação das bibliotecas
+import streamlit as st 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+import joblib
+from joblib import load
+
+
