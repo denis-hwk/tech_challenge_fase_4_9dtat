c641d6a052fde44adb4c5869f0ea8e7f4c116b30
denis-hwk <68127514+denis-hwk@users.noreply.github.com>
2025-12-12 19:32:06 -0300

    Teste SciKit Learn

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #Importação das bibliotecas
 import streamlit as st 
 import pandas as pd
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 import joblib
 from joblib import load
 
