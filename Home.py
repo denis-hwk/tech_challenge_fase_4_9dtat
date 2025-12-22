e49866a7f1a00596173c3b7726c72bf6a73a25e7
denis-hwk <68127514+denis-hwk@users.noreply.github.com>
2025-12-21 22:30:11 -0300

    Ajustando o nome da página principal

--- Home.py
+++ Home.py
@@ -11,5 +11,5 @@ import plotly.express as px
 
 st.set_page_config(page_title="FIAP Tech Challenge 4 - Dataviz and Production Models")
 st.title("FIAP Tech Challenge 4 - Dataviz and Production Models")
-st.write("Essa página é dedicada a apresentar uma solução de visualização dos dados encontrando no dataset 'Obesity' e um modelo preditivo em produção." \
-"\nTodo o conteúdo presente nessa página foi desenvolvido por Dênis Korin")
+st.write("Essa página é dedicada a apresentar uma solução de visualização dos dados encontrando no dataset 'Obesity' e um modelo preditivo em produção.")
+st.write("\nTodo o conteúdo presente nessa página foi desenvolvido por Dênis Korin")
