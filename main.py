2dfcc308b85aee396d78e53d0a726924a8fa97ce
denis-hwk <68127514+denis-hwk@users.noreply.github.com>
2025-12-21 22:22:30 -0300

    Ajuste main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="FIAP Tech Challenge 4 - Dataviz and Production Models")
 st.title("FIAP Tech Challenge 4 - Dataviz and Production Models")
-st.write("Essa página é dedicada a apresentar uma solução de visualização dos dados encontrando no dataset 'Obesity' e um modelo preditivo em produção")
+st.write("Essa página é dedicada a apresentar uma solução de visualização dos dados encontrando no dataset 'Obesity' e um modelo preditivo em produção." \
+"\nTodo o conteúdo presente nessa página foi desenvolvido por Dênis Korin")
