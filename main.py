d9701794b815eb0ea00e3df9ce274647d036e256
denis-hwk <68127514+denis-hwk@users.noreply.github.com>
2025-12-21 22:26:10 -0300

    Ajuste dos apps

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import streamlit as st
+#Importação das bibliotecas
+import streamlit as st 
+import pandas as pd
+from utils import MinMaxScalerFeatures, OneHotEncodingFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+import joblib
+from joblib import load
+import time
+import plotly.express as px
 
 st.set_page_config(page_title="FIAP Tech Challenge 4 - Dataviz and Production Models")
 st.title("FIAP Tech Challenge 4 - Dataviz and Production Models")
